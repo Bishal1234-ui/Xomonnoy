5f7292027b502fa371760b233e3974e6e9e38fe3
Dishanka kalita <dishankakalita@gmail.com>
2023-03-13 20:24:16 +0530

    ok

--- src/home.jsx
+++ src/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     const { loginWithRedirect } = useAuth0();
     return (
         <div>
-        <h1>Welcome to My App</h1>
+        <h1>Welcome to the home page of our App</h1>
             <button onClick={() => loginWithRedirect()}>Log In</button>
 
         </div>
